ba6c6d9b3e5db1b5d9a3c6cb66c5262339982034
Aditya Jyotiraditya Sahoo <90087194+thesunjrs@users.noreply.github.com>
2022-12-26 12:00:37 +0530

    Add files via upload

---
+++ 2nd Semester/Chinmaya Sir/Lab 8 Exception Handling/eh2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class eh2
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner("Aditya Narayan Panda");
+		System.out.println(" "+sc.next());
+		System.out.println(" "+sc.ioException());
+	}
+}
